31b3e203c1cf23055a575757290079efcec0a6dd
q1ay2.xyz <66400989+p1ay2invokio@users.noreply.github.com>
2025-01-11 10:09:29 +0700

    Add files via upload

---
+++ package.bat
@@ -0,0 +1,5 @@
+@echo off
+start "" cmd /c "cd ./client && npm install"
+start "" cmd /c "cd ./stock-management-main && npm install"
+
+exit
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+start "" cmd /c "cd ./client && start start.bat"
+start "" cmd /c "cd ./stock-management-main && start start.bat"
+
+exit
